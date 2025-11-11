d00b5c92dcc657f48a0e19fb645c64790a00696c
Cuong. Duong Manh <cuongpigerr@gmail.com>
2025-11-11 23:07:52 +0700

    This is Manh Cuong's commit.

--- chap04/script.sql
+++ chap04/script.sql
@@ -1,11 +1,16 @@
-SELECT f.flight_no,
-f.actual_departure,
-count(passenger_id) passengers
-FROM flight f
-JOIN booking_leg bl ON bl.flight_id = f.flight_id
-JOIN passenger p ON p.booking_id=bl.booking_id
-WHERE f.departure_airport = 'JFK'
-AND f.arrival_airport = 'ORD'
-AND f.actual_departure BETWEEN
-'2023-08-10' and '2023-08-13'
-GROUP BY f.flight_id, f.actual_departure;
+-- 4-1. A query selecting the number of passengers on specific flights
+SELECT
+    f.flight_no,
+    f.actual_departure,
+    count(passenger_id) passengers
+FROM
+    flight f
+    JOIN booking_leg bl ON bl.flight_id = f.flight_id
+    JOIN passenger p ON p.booking_id = bl.booking_id
+WHERE
+    f.departure_airport = 'JFK'
+    AND f.arrival_airport = 'ORD'
+    AND f.actual_departure BETWEEN '2023-08-10' and '2023-08-13'
+GROUP BY
+    f.flight_id,
+    f.actual_departure;
